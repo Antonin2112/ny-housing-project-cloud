5f9bd32ff9b2587491b983a4db7317a0ae09fe48
Antonin2112 <antonin.ronseray@orange.fr>
2026-03-12 22:17:17 +0100

    Ajout connection pool

--- app/app.py
+++ app/app.py
@@ -1,37 +1,48 @@
 import os 
 from flask import Flask, request
+from psycopg2 import pool 
 from psycopg2 import connect, sql
 
 app = Flask(__name__)
 
+connection_pool = pool.SimpleConnectionPool(
+    minconn=2,   # connexions ouvertes dès le départ
+    maxconn=10,  # maximum de connexions simultanées vers PG
+    dbname=os.environ.get('POSTGRES_DB'),
+    user=os.environ.get('POSTGRES_USER'),
+    password=os.environ.get('POSTGRES_PASSWORD'),
+    host='db',
+    port='5432'
+)
 def get_db_connection():
-    conn = connect(
-        dbname=os.environ['POSTGRES_DB'],
-        user=os.environ['POSTGRES_USER'],
-        password=os.environ['POSTGRES_PASSWORD'],
-        host='db',
-        port='5432'
-    )
-    return conn 
+    return connection_pool.getconn()
+
+def release_connection(conn):
+    connection_pool.putconn(conn)
+
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT COUNT(*) FROM housing;')
-    count = cur.fetchone()[0]
-    cur.close()
-    conn.close()
+    try:
+        cur = conn.cursor()
+        cur.execute('SELECT COUNT(*) FROM housing;')
+        count = cur.fetchone()[0]
+        cur.close()
+    finally:
+        release_connection(conn)
     return f'Connected to PostgreSQL database. Number of rows in housing table: {count}'
 
 @app.route('/stats')
 def stats():
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT LOCALITY, AVG(PRICE) AS moy FROM housing GROUP BY LOCALITY ORDER BY moy DESC;')
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
+    try: 
+        cur = conn.cursor()
+        cur.execute('SELECT LOCALITY, AVG(PRICE) AS moy FROM housing GROUP BY LOCALITY ORDER BY moy DESC;')
+        rows = cur.fetchall()
+        cur.close()
+    finally:
+        release_connection(conn)
     response= "<h3>Average price of housing by locality:</h3><ul>"
     for row in rows:
         response += f"<li>{row[0]}: {row[1]:.2f}</li>"
@@ -56,7 +67,7 @@ def houses():
         rows = cur.fetchall()
         cur.close()
     finally:
-        conn.close()
+        release_connection(conn)
 
     result = f"<h3>Houses in {locality}</h3><ul>"
     for r in rows:
